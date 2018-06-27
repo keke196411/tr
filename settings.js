05bdfc889910edd38ebb1c3b9e3e9cd1b0589770
keke196411 <279072500@qq.com>
2018-06-27 20:15:39 +0800

    tr

---
+++ settings.js
@@ -0,0 +1,61 @@
+{
+"emmet.syntaxProfiles": {
+"vue-html": "html",
+"vue": "html"
+},
+"files.associations": {
+"*.vue": "vue"
+},
+"emmet.includeLanguages": {
+"vue": "html",
+"vue-html": "html"
+},
+"beautify.language": {
+"js": {
+"type": [
+"javascript",
+"json",
+"typescript"
+],
+"filename": [
+".jshintrc",
+".jsbeautify",
+".jsbeautifyrc"
+]
+},
+"css": [
+"css",
+"scss",
+"less"
+],
+"html": [
+"htm",
+"html",
+"vue"
+]
+},
+"eslint.alwaysShowStatus": true,
+"eslint.autoFixOnSave": true,
+"eslint.validate": [
+{
+"language": "javascript",
+"autoFix": true
+},
+"javascriptreact",
+{
+"language": "vue",
+"autoFix": true
+},
+{
+"language": "html",
+"autoFix": true
+}
+],
+"vetur.grammar.customBlocks": {
+"i18n": "yaml"
+},
+"git.autofetch": true,
+"fileheader.Author": "lxt",
+"fileheader.LastModifiedBy": "lxt",
+"fileheader.tpl": "/*\r\n * @Author: {author} \r\n * @Date: {createTime} \r\n * @Last Modified by: {lastModifiedBy} \r\n * @Last Modified time: {updateTime} \r\n */\r\n"
+}
